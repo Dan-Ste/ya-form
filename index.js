9f940213075ee66d2e14a51c4ddfd723e6e88edd
Dan Steshenko <luciferburn26@gmail.com>
2017-07-31 23:05:57 +0300

    More logic - Add documentation - Fix bug in phone validation - Rewrite getData and setData - Closed htmlElement in myForm object

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
 const MyForm = (function () {
-  let mockedResponse = success;
 
+  /**
+   * @type {HTMLElement}
+   */
+  const _fromElem = document.getElementById('myForm');
+
+  /**
+   * @type {HTMLElement}
+   */
+  const _resultContainer = document.getElementById('resultContainer');
+
+  let _mockedResponse = success;
+
+  /**
+   * Add listener on form submit
+   */
+  const init = () => {
+    _fromElem.addEventListener('submit', submit);
+  };
+
+  /**
+   * Gets data from form fields
+   * @param {undefined}
+   * @return {Object} - data from the form in the form - inputName: inputValue
+   */
   const getData = () => {
-    const formElem = document.getElementById('myForm');
+    const data = {};
 
-    return {
-      'fio': formElem['fio'].value,
-      'email': formElem['email'].value,
-      'phone': formElem['phone'].value
-    }
+    [].forEach.call(_fromElem.elements, elem => {
+      if (elem.type !== 'submit') {
+        data[elem.name] = elem.value;
+      }
+    });
+
+    return data;
   };
 
-  const setData = ({
-    fio,
-    email,
-    phone
-  }) => {
-    const formElem = document.getElementById('myForm');
-
-    formElem['fio'].value = fio;
-    formElem['email'].value = email;
-    formElem['phone'].value = phone;
+  /**
+   * Sets data to form fields
+   * @param {Object} data - data to be set to the form
+   * @return {undefined}
+   */
+  const setData = (data) => {
+    Object.keys(data).forEach(key => {
+      _fromElem[key].value = data[key];
+    });
   };
 
-  const validate = formData => {
+  /**
+   * @typedef {Object} ValidationResult
+   * @property {Boolean} isValid - result of the validation
+   * @property {Array} errorFields - form fields with error
+   * 
+   * Validates form
+   * @param {undefined}
+   * @return {ValidationResult} - validation meta info 
+   * 
+   */
+  const validate = () => {
+    const formData = getData();
     const errorFields = [];
     const isFioValid = _validateFio(formData['fio']);
     const isEmailValid = _validateEmail(formData['email']);
@@ -39,72 +74,97 @@ const MyForm = (function () {
     }
   };
 
-  const submit = event => {
-    event.preventDefault();
+  /**
+   * Handles from submit
+   * @param {undefined}
+   * @return {undefined}
+   */
+  const submit = () => {
+    _fromElem['fio'].className = '';
+    _fromElem['email'].className = '';
+    _fromElem['phone'].className = '';
 
-    const formElem = event.target;
-
-    formElem['fio'].className = '';
-    formElem['email'].className = '';
-    formElem['phone'].className = '';
-
-    const formData = getData();
     const {
       isValid,
       errorFields
-    } = validate(formData);
+    } = validate();
 
     if (isValid) {
-      // XHR запросы нельзя выполнять локально по протоколу file:///
-      // Поэтому результат берется из глобальных объектов
-      _processResponse(mockedResponse);
+      _fromElem.elements.submitButton.disabled = true;
 
       setData({
         'fio': '',
         'email': '',
         'phone': ''
       });
+
+      // XHR запросы нельзя выполнять по протоколу file:///
+      // Можно поднять локальный сервер для решения задачи,
+      // но я решил сделать эмуляцию общения с сервером.
+      _processResponse(mockedResponse);
     } else {
-      errorFields.forEach(error => {
-        formElem[error].className = 'ya-input__error';
+      errorFields.forEach(errorField => {
+        _fromElem[errorField].className = 'ya-input__error';
       });
     }
+
+    event.preventDefault();
   }
 
-  const _validateFio = value => {
-    return false;
-  };
+  /**
+   * Validates FIO field
+   * @param {string} value - FIO field value
+   * @return {Boolean} - validation result
+   */
+  const _validateFio = value => value && value.trim().split(' ').length === 3;
 
+  /**
+   * Validates Email field
+   * @param {string} value - email field value
+   * @return {Boolean} - validation result
+   */
   const _validateEmail = value => {
     const emailRegExp = /^[a-z0-9]+\.[a-z0-9]+@yandex\.(ru|ua|kz|by|com)|ya\.ru/;
 
     return value && emailRegExp.test(value);
   };
 
+  /**
+   * Validates phone field
+   * @param {string} value - phone field value
+   * @return {Boolean} - validation result 
+   */
   const _validatePhone = value => {
-    const phoneRegExp = /^(\+7)\(\d{3}\)\d{3}-\d{2}-\d{2}/;
+    const phoneRegExp = /^(\+7)\(\d{3}\)\d{3}\-\d{2}\-\d{2}/;
 
-    return value && phoneRegExp.test(value) && _stringSum(value) > 30;
+    return value && phoneRegExp.test(value) && _stringSum(value) < 30;
   };
 
   const _processResponse = response => {
-    const resultContainer = document.getElementById('resultContainer');
-
-    resultContainer.className = '';
+    _resultContainer.className = '';
+    _resultContainer.innerHTML = '';
 
     switch (response.status) {
       case 'success':
-        resultContainer.className = 'success';
+        _resultContainer.className = 'success';
+        _resultContainer.innerHTML = 'Success';
         break;
       case 'error':
-        resultContainer.className = 'error';
+        _resultContainer.className = 'error';
+        _resultContainer.innerHTML = response.reason;
         break;
       case 'progress':
-        resultContainer.className = 'progress';
+        _resultContainer.className = 'progress';
+        _resultContainer.innerHTML = 'Progress...'
         break;
     }
   };
 
+  /**
+   * Sums integers in a strings
+   * @param {string} string - string with numbers to be summed
+   * @return {Number} - sum of the string's numbers
+   */
   const _stringSum = string => {
     const int = x => {
       const number = parseInt(x, 10);
@@ -113,18 +173,19 @@ const MyForm = (function () {
         return number;
       }
     }
+    const notUndefined = n => n != undefined;
     const sum = (a, b) => a + b;
 
-    return string.split('').map(int).reduce(sum);
+    return string.split('').map(int).filter(notUndefined).reduce(sum);
   };
 
   return {
     getData,
     setData,
     validate,
-    submit
+    submit,
+    init
   }
 }());
 
-const formElem = document.getElementById('myForm');
-formElem.addEventListener('submit', MyForm.submit);
+MyForm.init();
